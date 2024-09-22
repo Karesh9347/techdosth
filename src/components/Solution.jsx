a12c742e585311160fb61168d30cefb99d83c297
pattapu naresh <161163501+Karesh9347@users.noreply.github.com>
2024-09-22 09:58:55 +0530

    Add files via upload

---
+++ src/components/Solution.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import Navb from './Navb';
+import Footer from './Footer';
+import { Container, Spinner } from 'react-bootstrap';
+
+const Solution = () => {
+  const { id } = useParams();
+  const [question, setQuestion] = useState(null);
+
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/aquestions/${id}`);
+        setQuestion(response.data);
+        console.log(response.data)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchQuestion();
+  }, [id]);
+
+  return (
+    <div>
+      <Navb/>
+      {question ? (
+        <Container>
+          <strong style={{color:"red"}}>{question.questionName}</strong>
+          <p>Description: {question.description}</p>
+         <div style={{width:"100%",height:"100%"}}>
+         <img src={question.solution} alt='solution' style={{width:"90%",height:"90%"}}/>
+         </div>
+        </Container>
+      ) : (
+       <div>
+         <Spinner/><p>loading</p>
+        </div>
+      )}
+      <Footer/>
+    </div>
+  );
+};
+
+export default Solution;
