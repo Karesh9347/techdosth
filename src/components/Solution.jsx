9cacb40836a4d4a1f49ba68aafcc06dd12f7682e
pattapu naresh <161163501+Karesh9347@users.noreply.github.com>
2024-09-26 10:26:19 +0530

    Update Solution.jsx

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -4,16 +4,18 @@ import axios from 'axios';
 import Navb from './Navb';
 import Footer from './Footer';
 import { Container, Spinner } from 'react-bootstrap';
-
+import '../css/solution.css'
 const Solution = () => {
   const { id } = useParams();
   const [question, setQuestion] = useState(null);
+
 
   useEffect(() => {
     const fetchQuestion = async () => {
       try {
         const response = await axios.get(`https://techdosth-backend-1.onrender.com/aquestions/${id}`);
         setQuestion(response.data);
+       
         console.log(response.data)
       } catch (err) {
         console.log(err);
@@ -21,15 +23,23 @@ const Solution = () => {
     };
     fetchQuestion();
   }, [id]);
-
+  
   return (
     <div>
       <Navb/>
       {question ? (
         <Container>
+          <strong className='head'>Question:</strong>
+        <br/>
           <strong style={{color:"red"}}>{question.questionName}</strong>
-          <p>Description: {question.description}</p>
+          <br />
+          <strong className='head'>Description:</strong>
+        
+          <p> {question.description}</p>
+       
          <div style={{width:"100%",height:"100%"}}>
+          <strong>Solutions:</strong>
+        <hr/>
          <img src={question.solution} alt='solution' style={{width:"90%",height:"90%"}}/>
          </div>
         </Container>
