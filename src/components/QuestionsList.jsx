f88be9b1e6a4576e3712884cf7ad06feac2a5899
pattapu naresh <161163501+Karesh9347@users.noreply.github.com>
2024-09-28 00:50:45 +0530

    Update QuestionsList.jsx

--- src/components/QuestionsList.jsx
+++ src/components/QuestionsList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Container, Table, Alert, Row, Col, Button, Spinner } from 'react-bootstrap';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import '../css/qlist.css'
 
 const QuestionsList = () => {
   const [questions, setQuestions] = useState([]);
@@ -13,20 +14,18 @@ const QuestionsList = () => {
       try {
         setLoading(true);
         const response = await axios.get('https://techdosth-backend-1.onrender.com/questions');
-      
         setQuestions(response.data);
-       
       } catch (err) {
         setError('Error fetching questions: ' + err.message);
         console.error("Error:", err.message);
       } finally {
         setLoading(false);
-        
       }
     };
 
     fetchQuestions();
-  }, []); // Fetch data on component mount
+  }, []);
+
   function getColorByDifficulty(difficulty) {
     switch (difficulty.toLowerCase()) {
       case 'easy':
@@ -36,37 +35,53 @@ const QuestionsList = () => {
       case 'hard':
         return 'red';
       default:
-        return 'black'; // default color if difficulty is undefined
+        return 'black';
     }
   }
-  
+
+  const spinnerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100px', // Set a specific height for spinner alignment
+  };
+
   return (
     <Container style={{ marginTop: "20px" }}>
       <Row className="mb-3">
         <Col>
           <center>
-          <h4 className="heading" style={{color:"green"}}>Data Structures and Algorithms Questions with Solutions and Explanations</h4>
-          </center></Col>
-        
+          <h5 className="heading">DSA Questions with Solutions </h5>
+          </center>
+        </Col>
       </Row>
-      
+
       {error && <Alert variant="danger">{error}</Alert>}
-     
-        <Row>
-          <Col>
-            <div className="table-responsive">
-              <Table striped bordered hover>
-                <thead className="bg-warning custom-head-color">
-                  <tr >
-                    <th>ID</th>
-                    <th>Question</th>
-                    <th>Problem Link</th>
-                    <th>solution</th>
-                    <th>difficulty</th>
+
+      <Row>
+        <Col>
+          <div className="table-responsive">
+            <Table striped bordered hover>
+              <thead className="bg-warning custom-head-color">
+                <tr>
+                  <th>ID</th>
+                  <th>Question</th>
+                  <th>Problem Link</th>
+                  <th>Solution</th>
+                  <th>Difficulty</th>
+                </tr>
+              </thead>
+              <tbody>
+                {loading ? (
+                  <tr>
+                    <td colSpan={5}>
+                      <div style={spinnerStyle}>
+                        <Spinner style={{ width: '3rem', height: '3rem' }} animation="border" variant="primary" />
+                      </div>
+                    </td>
                   </tr>
-                </thead>
-                {!loading?<tbody>
-                  {questions.map((question, index) => (
+                ) : (
+                  questions.map((question, index) => (
                     <tr key={question._id}>
                       <td>{index + 1}</td>
                       <td>{question.QuestionName}</td>
@@ -76,20 +91,19 @@ const QuestionsList = () => {
                         </a>
                       </td>
                       <td>
-                        <Link to={`/questions/${question._id}`}>solution</Link>
+                        <Link to={`/questions/${question._id}`}>Solution</Link>
                       </td>
                       <td style={{ color: getColorByDifficulty(question.difficulty) }}>
-  {question.difficulty}
-</td>
-
+                        {question.difficulty}
+                      </td>
                     </tr>
-                  ))}
-                </tbody>:<center><Spinner/></center> }
-              </Table>
-            </div>
-          </Col>
-        </Row>
-     
+                  ))
+                )}
+              </tbody>
+            </Table>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
