8d38c048c09b683216958c29eb7630b41340db99
pattapu naresh <161163501+Karesh9347@users.noreply.github.com>
2024-10-02 12:25:19 +0530

    Update Greedy.jsx

--- src/components/Greedy.jsx
+++ src/components/Greedy.jsx
@@ -26,7 +26,7 @@ const Greedy = () => {
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
-        const response = await axios.get('https://techdosth-backend-1.onrender.com/questions');
+        const response = await axios.get('https://techdosth-backend.onrender.com/questions');
         // Filter questions with the "greedy" hashtag
         
         const greedyQuestions = response.data.filter(question =>
