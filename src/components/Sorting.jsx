d2952e26d5fc4ca043b52c45ce7db3a5a494cae3
pattapu naresh <161163501+Karesh9347@users.noreply.github.com>
2024-10-02 12:31:35 +0530

    Update Sorting.jsx

--- src/components/Sorting.jsx
+++ src/components/Sorting.jsx
@@ -15,7 +15,7 @@ const Sorting = () => {
   useEffect(() => {
     const fetchQuestions = async () => {
       try {
-        const response = await axios.get('https://techdosth-backend-1.onrender.com/questions');
+        const response = await axios.get('https://techdosth-backend.onrender.com/questions');
         
         // Filter questions with the "sorting" hashtag
         const sortingQuestions = response.data.filter(question =>
