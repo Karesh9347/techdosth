f26c2dfe9a0265494c2f72db4dcff11b77a0687d
pattapu naresh <161163501+Karesh9347@users.noreply.github.com>
2024-10-10 02:06:46 +0530

    Update Navb.jsx

--- src/components/Navb.jsx
+++ src/components/Navb.jsx
@@ -1,80 +1,78 @@
-import React from 'react';
-import { Navbar, Nav, Container, Offcanvas } from 'react-bootstrap';
-import { House, Book, Person, Pen, Code, CodeSquare, Lightbulb } from 'react-bootstrap-icons';
-import '../css/nav.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Navbar, Nav, Container, Offcanvas } from "react-bootstrap";
+import { House, Book, Person, Pen, CodeSquare, Lightbulb } from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
+import "../css/nav.css";
 
 function Navb() {
-  const user = JSON.parse(localStorage.getItem("user")) || null; // Fallback to null if parsing fails
- 
+  const user = JSON.parse(localStorage.getItem("user")) || null;
 
   return (
-    <Navbar bg="light" expand="lg" className="mb-3">
+    <Navbar bg="light" expand="lg">
       <Container fluid>
         <Navbar.Brand href="#home">
           <div className="logo-container">
             <div className="logo-icon">T</div>
-            <div className="logo-text">ech<span>Dosth</span></div>
+            <div className="logo-text">
+              ech<span>Dosth</span>
+            </div>
           </div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="offcanvasNavbar" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Navbar.Offcanvas
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-            placement="end"
-          >
+          <Navbar.Offcanvas id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" placement="end">
             <Offcanvas.Header closeButton>
               <Offcanvas.Title id="offcanvasNavbarLabel">
                 <div className="logo-container">
                   <div className="logo-icon">T</div>
-                  <div className="logo-text">ech<span>Dosth</span></div>
+                  <div className="logo-text">
+                    ech<span>Dosth</span>
+                  </div>
                 </div>
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link as={Link} to="/">
-                  <div style={{ display: "flex" }}>
-                    <CodeSquare className="me-2" size={25} />
-                    <h5>DSA</h5>
-                  </div>
-                </Nav.Link>
-                <Nav.Link as={Link} to="/aptitude">
-                  <div style={{ display: "flex" }}>
-                    <Lightbulb className="me-2" size={25} />
-                    <h5>Aptitude</h5>
-                  </div>
-                </Nav.Link>
-                <Nav.Link as={Link} to="/contest">
-                  <div style={{ display: "flex" }}>
-                    <Pen className="me-2" size={25} />
-                    <h5>Contest</h5>
-                  </div>
-                </Nav.Link>
-                <Nav.Link as={Link} to="/courses">
-                  <div style={{ display: "flex" }}>
-                    <Book className="me-2" size={25} />
-                    <h5>Courses</h5>
-                  </div>
-                </Nav.Link>
-                <div>
-                  {user ? (
-                    <Nav.Link as={Link} to='/profile'>
-                      <div style={{ display: "flex" }}>
-                        <Person className="me-2" size={25} />
-                        <h5>{user.user.username}</h5>
-                      </div>
-                    </Nav.Link>
-                  ) : (
-                    <Nav.Link as={Link} to='/login'>
-                      <div style={{ display: "flex" }}>
-                        <Person className="me-2" size={25} />
-                        <h5>Account</h5>
-                      </div>
-                    </Nav.Link>
-                  )}
-                </div>
+                {[
+                  { to: "/", icon: <House size={20} />, label: "Home" },
+                  {
+                    to: "/dsa",
+                    icon: <CodeSquare size={20} />,
+                    label: "DSA",
+                    description: "Practice data structures and algorithms problems for interviews",
+                  },
+                  {
+                    to: "/aptitude",
+                    icon: <Lightbulb size={20} />,
+                    label: "Aptitude",
+                    description: "Learn aptitude to crack first round of interview",
+                  },
+                  {
+                    to: "/sql-queries",
+                    icon: <CodeSquare size={20} />,
+                    label: "SQL Queries",
+                    description: "Practice DBMS and SQL to crack jobs like data analyst and full stack developer",
+                  },
+                  { to: "/contest", icon: <Pen size={20} />, label: "Contest" },
+                  { to: "/courses", icon: <Book size={20} />, label: "Interviews MCQ's" },
+                  {
+                    to: user ? "/profile" : "/login",
+                    icon: <Person size={20} />,
+                    label: user ? user.user.username : "Account",
+                  },
+                ].map((navItem, index) => (
+                  <Nav.Link as={Link} to={navItem.to} key={index}>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      {navItem.icon}
+                      <h6 style={{ margin: "0 10px" }}>{navItem.label}</h6>
+                      {navItem.description && (
+                        <div className="dropdown-content">
+                          <span className="text-dark">{navItem.description}</span>
+                        </div>
+                      )}
+                    </div>
+                  </Nav.Link>
+                ))}
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
