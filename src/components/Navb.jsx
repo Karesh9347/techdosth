887a4245d702d7221e952968d3554f4a2a0c6007
pattapu naresh <161163501+Karesh9347@users.noreply.github.com>
2024-10-24 01:05:05 +0530

    Update Navb.jsx

--- src/components/Navb.jsx
+++ src/components/Navb.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Navbar, Nav, Container, Offcanvas } from "react-bootstrap";
-import { House, Book, Person, Pen, CodeSquare, Lightbulb } from "react-bootstrap-icons";
+import { Navbar, Nav, Container, Offcanvas, Dropdown } from "react-bootstrap";
+import { House, Book, Person ,Pen} from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 import "../css/nav.css";
 
@@ -35,43 +35,77 @@ function Navb() {
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 {[
                   { to: "/", icon: <House size={20} />, label: "Home" },
-                  {
-                    to: "/dsa",
-                    icon: <CodeSquare size={20} />,
-                    label: "DSA",
-                    description: "Practice data structures and algorithms problems for interviews",
+                  { 
+                    label: "Practice", 
+                    icon: <Pen size={20} />,
+                    dropdown: true,
                   },
-                  {
-                    to: "/aptitude",
-                    icon: <Lightbulb size={20} />,
-                    label: "Aptitude",
-                    description: "Learn aptitude to crack first round of interview",
+                
+          
+               
+                  { 
+                    label: "Interview preparation", 
+                    icon: <Book size={20} />,
+                    dropdown: true,
                   },
-                  {
-                    to: "/sql-queries",
-                    icon: <CodeSquare size={20} />,
-                    label: "SQL Queries",
-                    description: "Practice DBMS and SQL to crack jobs like data analyst and full stack developer",
-                  },
-                  { to: "/contest", icon: <Pen size={20} />, label: "Contest" },
-                  { to: "/courses", icon: <Book size={20} />, label: "Interviews MCQ's" },
+                 
                   {
                     to: user ? "/profile" : "/login",
                     icon: <Person size={20} />,
                     label: user ? user.user.username : "Account",
                   },
                 ].map((navItem, index) => (
-                  <Nav.Link as={Link} to={navItem.to} key={index}>
-                    <div style={{ display: "flex", alignItems: "center" }}>
-                      {navItem.icon}
-                      <h6 style={{ margin: "0 10px" }}>{navItem.label}</h6>
-                      {navItem.description && (
-                        <div className="dropdown-content">
-                          <span className="text-dark">{navItem.description}</span>
+                  <div key={index}>
+                    {navItem.dropdown ? (
+                      <Dropdown>
+                        <Dropdown.Toggle as={Nav.Link}>
+                          <div style={{ display: "flex", alignItems: "center" }}>
+                            {navItem.icon}
+                            <h6 style={{ margin: "0 10px" }}>{navItem.label}</h6>
+                            
+                          </div>
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu className="custom-dropdown">
+  {navItem.label === "Interview preparation" && (
+    <>
+      <Dropdown.Item as={Link} to="/ai-interview">
+        Mock Interviews  <br/>Simulate real-time interviews with AI-driven feedback.
+      </Dropdown.Item>
+      <Dropdown.Item as={Link} to="/courses">
+        Interview MCQs with Explanation  <br/> Multiple-choice questions with detailed explanations.
+      </Dropdown.Item>
+    </>
+  )}
+  {navItem.label === "Practice" && (
+    <>
+      <Dropdown.Item as={Link} to="/dsa">
+        DSA Problems  <br/> Practice data structures and algorithms challenges.
+      </Dropdown.Item>
+      <Dropdown.Item as={Link} to="/sql-queries">
+        SQL Queries  <br/> Solve problems to improve your SQL query skills.
+      </Dropdown.Item>
+      <Dropdown.Item as={Link} to="/aptitude">
+        Aptitude Questions 
+        <br/>
+         Hone your logical, quantitative, and verbal reasoning.
+      </Dropdown.Item>
+      <Dropdown.Item as={Link} to="/contest">
+        Contests  <br/> Participate in coding contests to test your skills.
+      </Dropdown.Item>
+    </>
+  )}
+</Dropdown.Menu>
+
+                      </Dropdown>
+                    ) : (
+                      <Nav.Link as={Link} to={navItem.to}>
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                          {navItem.icon}
+                          <h6 style={{ margin: "0 10px" }}>{navItem.label}</h6>
                         </div>
-                      )}
-                    </div>
-                  </Nav.Link>
+                      </Nav.Link>
+                    )}
+                  </div>
                 ))}
               </Nav>
             </Offcanvas.Body>
